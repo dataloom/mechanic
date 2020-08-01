43a695cfd954e7a9a19107f1dec7608ec6b74b07
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-08-01 00:29:23 -0700

    Make it pass on bamboo too

---
+++ src/test/kotlin/com/openlattice/mechanic/MechanicTests/MechanicTests.kt
@@ -0,0 +1,14 @@
+package com.openlattice.mechanic.MechanicTests
+
+import org.junit.Test
+
+/**
+ *
+ * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
+ */
+class MechanicTests {
+    @Test
+    fun passOnBamboo() {
+
+    }
+}
